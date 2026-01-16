8e5a0452931025ca46a861f01242310d8e833877
Goala21 <samuel.lanzon2002@gmail.com>
2026-01-16 11:23:01 +0100

    Update contact.js

--- Contact Us/contact.js
+++ Contact Us/contact.js
@@ -1,3 +1,16 @@
-function Validate(){
-        
+function checkinginput(event){
+    event.preventDefault();
+
+    let usernameInput = document.getElementById("username_input").value;
+    let surnameInput = document.getElementById("surname_input").value;
+    let contactInput = document.getElementById("contact_input").value;
+    let subjectInput = document.getElementById("subject_input").value;
+    let commentInput = document.getElementById("comment_input").value;
+
+    if ((usernameInput == "") || (subjectInput == "") || (surnameInput == "") || (contactInput == "") || (commentInput == "")){
+        alert("You on weed on smth bro? Type that shit in")
+    }
+
+
+
 }
